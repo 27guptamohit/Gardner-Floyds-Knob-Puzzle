e9f6ee24deef882326c18167df5c2751b639df8c
Mohit Gupta <44348351+27guptamohit@users.noreply.github.com>
2019-09-24 20:38:41 -0500

    Started to write the function of the board game's display.

--- Solution.py
+++ Solution.py
@@ -5,13 +5,12 @@
 import numpy as np
 import random
 
-
 m = int(input(print("Enter the number of rows:")))
 
 n = int(input(print("Enter the number of columns:")))
 
 
-def organized_view(puzzle_generator):
+def organized_view(puzzle_structure):
     """
     This function will accept the numpy array and display it's contents in an organized way.
     :param puzzle_generator:
@@ -26,11 +25,7 @@ def organized_view(puzzle_generator):
 
         print("      ".join(map(str, i)))
 
-
     print("----------------------------------------------------------------\n\n")
-
-
-
 
 
 def puzzle_structure_generator(m, n):
@@ -41,7 +36,6 @@ def puzzle_structure_generator(m, n):
     :return:  The final base structure of the puzzle
     """
 
-
     # Using list comprehension to generate the initial puzzle in the form of nested lists
 
     # Here "W" = West
@@ -51,7 +45,6 @@ def puzzle_structure_generator(m, n):
 
     a = [["W", "N", "S", "E"] for i in range(m)]
     puzzle_structure = [a for i in range(n)]
-
 
     puzzle_structure = np.array(puzzle_structure)
 
@@ -64,32 +57,21 @@ def puzzle_structure_generator(m, n):
     # South Wall, "S" == 0
     # East Wall, "E" == 0
 
-
     for i in range(n):
-
         # For North Wall, "N" = 0
         puzzle_structure[0][i][1] = 0
 
         # For South Wall, "S" = 0
-        puzzle_structure[m-1][i][2] = 0
-
+        puzzle_structure[m - 1][i][2] = 0
 
     for i in range(m):
-
-
-
         # For West Wall, "W" = 0
         puzzle_structure[i][0][0] = 0
 
         # For East Wall, "E" = 0
-        puzzle_structure[i][n-1][3] = 0
-
-
+        puzzle_structure[i][n - 1][3] = 0
 
     return puzzle_structure
-
-
-
 
 
 def random_entry_point(puzzle_structure):
@@ -111,7 +93,6 @@ def random_entry_point(puzzle_structure):
     two, the list which contains the solution coordinates.
     """
 
-
     entry_point = puzzle_structure[:, 0, 0]
 
     # I visited https://www.geeksforgeeks.org/python-select-random-value-from-a-list/ to find better method to randomly select an index.
@@ -119,84 +100,56 @@ def random_entry_point(puzzle_structure):
     random_entry_index = random.randrange(len(entry_point))
     entry_point[random_entry_index] = 1
 
-
-    exit_point = puzzle_structure[:, n-1, 3]
+    exit_point = puzzle_structure[:, n - 1, 3]
     random_exit_index = random.randrange(len(exit_point))
     exit_point[random_exit_index] = 1
 
-
-
     # Now I will replace the entry and exit points in the actual numpy array
     puzzle_structure[:, 0, 0] = entry_point
-    puzzle_structure[:, n-1, 3] = exit_point
-
+    puzzle_structure[:, n - 1, 3] = exit_point
 
     # I will also save the entry and exit points in a list that will act as a recorder for correct list solutions match.
     # Also, I will not randomly change the direction restrictions of the solution coordinates as they should be fixed.
 
-
-    solution_coordinate_sequence = [[random_entry_index, 0], [random_exit_index, n-1]]
-
+    solution_coordinate_sequence = [[random_entry_index, 0], [random_exit_index, n - 1]]
 
     return puzzle_structure, list(solution_coordinate_sequence)
 
 
-
-
 def solution_generator(solution_coordinate_sequence):
-
-
     coordinates = []
 
     entry_coordinate = solution_coordinate_sequence[0]
-    exit_coordinate =  solution_coordinate_sequence[-1]
-
+    exit_coordinate = solution_coordinate_sequence[-1]
 
     for i in range(n):
-
         coordinates.append([entry_coordinate[0], i])
-
-
 
     if entry_coordinate[0] > exit_coordinate[0]:
 
         for i in reversed(range(exit_coordinate[0], entry_coordinate[0])):
-
-
-            coordinates.append([ i ,n-1])
+            coordinates.append([i, n - 1])
 
     if entry_coordinate[0] < exit_coordinate[0]:
 
         for i in range(entry_coordinate[0] + 1, exit_coordinate[0] + 1):
-
-            coordinates.append([i, n-1])
-
-
+            coordinates.append([i, n - 1])
 
     return coordinates
 
 
-
-
 def random_direction_generator(puzzle_structure, solution_coordinate_sequence):
-
-
     p_structure = puzzle_structure
     solutions = solution_coordinate_sequence
     all_coordinates = []
     required_coordinates = []
-
-
 
     # Generating the possible coordinates for the m * n puzzle
 
     for i in range(m):
 
         for j in range(n):
-
-            all_coordinates.append([i,j])
-
-
+            all_coordinates.append([i, j])
 
     # Removing the coordinates that are in the solutions so that I would not mistakenly block the solution direction.
 
@@ -205,75 +158,89 @@ def random_direction_generator(puzzle_structure, solution_coordinate_sequence):
         if i not in solutions:
             required_coordinates.append(i)
 
-
     for i in required_coordinates:
 
-
         for j in range(4):
-
 
             # Now I am randomly generating one possible number, either '1' or '0'.
             # I learnt the below one line of code from the below link:
             # https://www.geeksforgeeks.org/python-select-random-value-from-a-list/
             # I the number that I generated is '0' then I will replace the individual element in the direction list to '0'.
 
-
             a = np.random.randint(2, size=1)
 
             if a == 0:
                 p_structure[i[0], i[1], j] = 0
 
-
     return p_structure
+
+
+def game_board(m, n, puzzle_structure, entry_coordinate, exit_coordinate):
+    # Making a board that only contains zero
+    # When I used the below code, I was facing error while replacing the player's position with the an array.
+    # I faced an error here earlier here which is described in the comment 39 in the below link:
+    # https://stackoverflow.com/questions/4674473/valueerror-setting-an-array-element-with-a-sequence
+    # Thus I decided not to use numpy array in this case and generate a normal array
+
+    # zero_board = np.full((m,n), ["0"])
+
+    zero_board = []
+
+    for i in range(m):
+        # I am adding eight spaces around both the sides of zero so that it would look presentable while printing the table.
+        zero_board.append(["        0        "] * n)
+
+    prev_x_coo = entry_coordinate[0]
+    prev_y_coo = entry_coordinate[1]
+
+    curr_x_coo = entry_coordinate[0]
+    curr_y_coo = entry_coordinate[1]
+
+    zero_board[curr_x_coo][curr_y_coo] = puzzle_structure[curr_x_coo, curr_y_coo]
+
+    return zero_board
 
 
 # Step 1 : Generating the base structure of the puzzle
 
-puzzle_structure = puzzle_structure_generator(m,n)
+puzzle_structure1 = puzzle_structure_generator(m, n)
 
 # In case if you search for the shape of the above generated puzzle when m * n = 3 * 3,
 # the shape would be ( 3, 3, 4 ) as I have designed it to be a multidimensional array.
 # Displaying the puzzle in an organized view:
-organized_view(puzzle_structure)
-
-
+organized_view(puzzle_structure1)
 
 # Step 2:
 # Generating random entry and exit points:
 
-puzzle_structure, solution_coordinate_sequence = random_entry_point(puzzle_structure)
-organized_view(puzzle_structure)
-
-
-
+puzzle_structure2, solution_coordinate_sequence1 = random_entry_point(puzzle_structure1)
+organized_view(puzzle_structure2)
 
 # Step 3:
 # Generating the correct coordinate sequence of puzzle solution after randomly generating the
 # entry and exit points:
 
 
-solution_coordinate_sequence = solution_generator(solution_coordinate_sequence)
+solution_coordinate_sequence2 = solution_generator(solution_coordinate_sequence1)
 
 # print("Below are the solution coordinates in sequence. \nIf any other sequence would be provided, you'll not reach the end point:")
 # print(solution_coordinate_sequence)
 # print("\n\n\n")
 
 
-
-
 # Step 4:
 
 # Now I will change the existing directions and generate random directions.
-puzzle_structure = random_direction_generator(puzzle_structure, solution_coordinate_sequence)
-organized_view(puzzle_structure)
+puzzle_structure3 = random_direction_generator(puzzle_structure2, solution_coordinate_sequence2)
+organized_view(puzzle_structure3)
 
+# Step 5:
 
+entry_coordinate = solution_coordinate_sequence2[0]
+exit_coordinate = solution_coordinate_sequence2[-1]
 
-
-
-
-
-
+zero_board1 = game_board(m, n, puzzle_structure3, entry_coordinate, exit_coordinate)
+organized_view(zero_board1)
 
 
 
